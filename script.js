415f4833bbc39393e9f0313a538e4b8220e80982
seiken0 <michalsstefanowicz@gmail.com>
2026-02-05 21:17:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function moveElmRand(elm) {
   elm.style.position = "absolute";
-  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+  elm.style.top = Math.floor(Math.random() * 70 + 10) + "%";
+  elm.style.left = Math.floor(Math.random() * 70 + 10) + "%";
 }
 
 const moveRandom = document.querySelector("#move-random");
